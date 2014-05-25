6bb8d886932fcad3885941ab7b55c3a08efbd49e
Palumbo <jhpalumbo@gmail.com>
2014-05-25 00:14:31 +0000

    added more code description to continue.php

--- continue.php
+++ continue.php
@@ -2,7 +2,22 @@
 <html> 
 
 <head> 
-<title>Continue</title> 
+	<title>Continue</title> 
+	<style type="text/css"> 
+	
+		body { 
+			font-family: sans-serif; 
+		}
+		
+		#code { 
+			font-family: monospace; 
+			background-color: lightgrey; 
+			padding: 10px; 
+			border: 1px solid; 
+			display: inline-block; 
+		}
+	
+	</style> 
 </head> 
 
 <body> 
@@ -23,18 +38,46 @@ while ($count <= 10) {
 	}
 ?>
 
+<h2>Here is what the code looks like...</h2>
+
+<div id="code">
+$count = 0; <br /> 
+<br /> 
+while ($count <= 10) { <br /> 
+&emsp;if ($count == 5) { <br /> 
+&emsp;&emsp;$count++; <br /> 
+&emsp;&emsp;continue; <br /> 
+&emsp;}<br /> 
+&emsp;echo $count . ", "; <br /> 
+}
+</div>
+
+<hr /> 
+
 <br />  <br /> 
 
 
 <?php
 
 for ($i=0; $i<=5; $i++) { 
-	if ($i % 2 == 0) { continue; }
+	if ($i % 2 == 0) { continue(1); }
 	for ($k=0; $k<=5; $k++) { 
+		if ($k==3) { continue(2); }
 		echo $i . "-" . $k . "<br />"; 
 	}
 }
 ?>
+
+<h2>Here is what the code looks like...</h2>
+
+<div id="code"> 
+	for ($i=0; $i<=5; $i++) { <br /> 
+	&emsp;if ($i % 2 == 0) { continue(1); } <br /> 
+	&emsp;for ($k=0; $k<=5; $k++) { <br />  
+	&emsp;&emsp;if ($k == 3) { continue(2); } <br /> 
+	&emsp;}<br /> 
+	}
+</div>
 
 </body> 
 </html> 
