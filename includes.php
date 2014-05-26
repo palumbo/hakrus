3f42114432c454c9cdf6a5d7f3682d43426d53d3
Palumbo <jhpalumbo@gmail.com>
2014-05-26 05:12:12 +0000

    adds redirect.php

---
+++ includes.php
@@ -0,0 +1,23 @@
+<?php
+	include("included_header.php"); 
+?>
+
+
+<!DOCTYPE HTML5>
+<html> 
+
+<head> 
+<title>Including and Requiring</title> 
+</head> 
+
+<body> 
+
+	<h1>Including and Requiring</h1>
+	
+	<p>
+	<code><a href="http://us1.php.net/manual/en/function.include.php"><code>include()</code></a>
+
+
+
+</body> 
+</html> 
