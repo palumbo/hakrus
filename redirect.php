ccaead9f686d96506a471900c4f474e37d50545a
Palumbo <jhpalumbo@gmail.com>
2014-05-26 05:13:09 +0000

    adds redirect.php which looks for logged_in value

--- redirect.php
+++ redirect.php
@@ -1,7 +1,20 @@
-<?php
-	header("Location: http://lynda.com"); 
-	exit; 
+<?php 
+
+	function redirect($page) { 
+		header("Location: {$page}"); 
+		exit; 
+	}
+	
+	$logged_in = $_GET['logged_in']; 
+	
+	if ($logged_in == 1) { 
+		redirect("index.php"); 
+	} else { 
+		redirect("http://thinkgeek.com"); 
+	}
+	
 ?>
+
 
 <!DOCTYPE HTML5>
 <html> 
