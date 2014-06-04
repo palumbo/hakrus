29216d74079141b448facc14148bf456dad2d315
Palumbo <jhpalumbo@gmail.com>
2014-06-04 07:50:39 +0000

    Revert "adds database.php and world.php - now working with mysql"
    This reverts commit 8f5d115ecace15ca9516b486a1a124382ba9f1de.

--- test.php
+++ test.php
@@ -1,59 +1,22 @@
-<?php 
-	// 1. make the connection 
-	$dbhost = "localhost"; 
-	$dbname = "widget_corp"; 
-	$dbuser = "widget_cms"; 
-	$dbpass = "foo.bar"; 
-	$connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname); 
-
-	if (mysqli_connect_errno()) { 
-		die("Database Connection Failed" . mysqli_connect_error()); 
-	}
-?>
-
-<?php 
-	// 2. perform the query
-	$query = "select * "; 
-	$query .= "from subjects "; 
-	$query .= "where visible = 1 ";
-	$query .= "order by position asc "; 
-
-	$result = mysqli_query($connection, $query); 
-
-	// test if query succeeded or failed
-	if (!$result) { 
-		die("Database query fialed"); 
-	} 
-?>
-
+<!DOCTYPE HTML5>
 <html> 
 
-<head>
-		<title>Database Connection Test</title> 
+<head> 
+<title>Test | Again</title> 
 </head> 
 
 <body> 
 
-	<pre> 
-	<?php 
-		// 3. user returned data
-		while ($row = mysqli_fetch_assoc($result)) { 
-			echo var_dump($row); 
-			echo "<br />"; 
-		}
-	?>
-	</pre>
-	
 	<?php
-		// 4. release the returned data
-		mysqli_free_result($result); 
+
+		include_once("vars.php"); 
+
+		echo "A {$color} {$fruit}"; 		
+
+		hello("Joseph"); 
+
+		hello("Lizzie"); 
 	?>
 
-</body> 
-</html> 
-
-<?php 
-	// 5. close the connection 
-	mysqli_close($connection); 
-?>
-
+</body>
+</body>
